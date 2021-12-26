ae21e91c9dcba6523f3c85b74f4b4165fc2d6d7e
baderanini <anini908@gmail.com>
2021-12-26 14:44:40 +0200

    Time: 8 ms (54.22%), Space: 38.6 MB (97.83%) - LeetHub

--- permutation-in-string/permutation-in-string.java
+++ permutation-in-string/permutation-in-string.java
@@ -1,31 +1,36 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int st = 0;
-        Map<Character, Integer> patternFreqs = fillFreqs(s1);
-        Map<Character, Integer> stringFreqs = new HashMap<>(patternFreqs);
+        int[] patternFreqs = new int[26];
+        int uniqueChars = fillFreqs(s1, patternFreqs);
         
         int matches = 0;
         for(int end = 0 ; end < s2.length() ; end++) {
             char curr = s2.charAt(end);
             
-            if(!patternFreqs.containsKey(curr)) {
+            if(patternFreqs[cIndex(curr)] == Integer.MIN_VALUE) {
                 matches = 0;
-                st = end+1;
-                stringFreqs = new HashMap<>(patternFreqs);
+                while(st < end+1) {
+                    char stChar = s2.charAt(st);
+                    if(patternFreqs[cIndex(stChar)] != Integer.MIN_VALUE) {
+                        patternFreqs[cIndex(stChar)]++;
+                    }
+                    st++;
+                }
             } else {
-                stringFreqs.put(curr, stringFreqs.get(curr) - 1);
-                if(stringFreqs.get(curr) == 0)
+                patternFreqs[cIndex(curr)]--;
+                if(patternFreqs[cIndex(curr)] == 0)
                     matches++;
-                else if(stringFreqs.get(curr) == -1)
+                else if(patternFreqs[cIndex(curr)] == -1)
                     matches--;
                 if(end - st + 1 == s1.length()) {
-                    if(matches == patternFreqs.size())
+                    if(matches == uniqueChars)
                         return true;
                     char stChar = s2.charAt(st);
-                    stringFreqs.put(stChar, stringFreqs.get(stChar) + 1);
-                    if(stringFreqs.get(stChar) == 0)
+                    patternFreqs[cIndex(stChar)]++;
+                    if(patternFreqs[cIndex(stChar)] == 0)
                         matches++;
-                    else if(stringFreqs.get(stChar) == 1)
+                    else if(patternFreqs[cIndex(stChar)] == 1)
                         matches--;
                     st++;
                 }
@@ -35,14 +40,26 @@ class Solution {
         return false;
     }
     
-    Map<Character, Integer> fillFreqs(String s) {
-        Map<Character, Integer> freqs = new HashMap<>();
+    int cIndex(char c) {
+        return c - 'a';
+    }
+    
+    int fillFreqs(String s, int[] freqs) {
+        int uniqueChars = 0;
+        
         for(int i = 0 ; i < s.length() ; i++) {
             char curr = s.charAt(i);
-            freqs.put(curr, freqs.getOrDefault(curr, 0) + 1);
+            if(freqs[cIndex(curr)] == 0)
+                uniqueChars++;
+            freqs[cIndex(curr)]++;
         }
         
-        return freqs;
+        for(int i = 0  ; i < 26 ; i++) {
+            if(freqs[i] == 0)
+             freqs[i] = Integer.MIN_VALUE;
+        }
+        
+        return uniqueChars;
     }
 }
 
