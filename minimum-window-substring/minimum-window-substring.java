2de029ea89fff8916ad8cba28919436b1afcf5d5
baderanini <anini908@gmail.com>
2021-12-26 15:56:00 +0200

    Time: 6 ms (79.45%), Space: 39.3 MB (68.05%) - LeetHub

--- minimum-window-substring/minimum-window-substring.java
+++ minimum-window-substring/minimum-window-substring.java
@@ -1,6 +1,7 @@
 class Solution {
     public String minWindow(String s, String t) {
-        Map<Character, Integer> freqs = fillFreqs(t);
+        int[] freqs = new int[52];
+        int uniqueChars = fillFreqs(t, freqs);
         
         int st = 0;
         int min = Integer.MAX_VALUE;
@@ -9,22 +10,23 @@ class Solution {
         int matches = 0;
         for(int end = 0 ; end < s.length() ; end++) {
             char curr = s.charAt(end);
-            if(freqs.containsKey(curr)) {
-                freqs.put(curr, freqs.get(curr) - 1);
-                if(freqs.get(curr) == 0)
+            if(freqs[cIndex(curr)] != Integer.MIN_VALUE) {
+                freqs[cIndex(curr)]--;
+                if(freqs[cIndex(curr)] == 0)
                      matches++;
             }
             
-            while(matches == freqs.size() && st <= end) {
+            while(matches == uniqueChars && st <= end) {
                 char stChar = s.charAt(st);
                 if(end - st + 1 < min) {
                     min = end - st + 1;
                     minSt = st;
                     minEnd = end;
                 }
-                if(freqs.containsKey(stChar)) {
-                    freqs.put(stChar, freqs.get(stChar) + 1);
-                    if(freqs.get(stChar) == 1)
+                if(freqs[cIndex(stChar)] != Integer.MIN_VALUE) {
+                    
+                    freqs[cIndex(stChar)]++;
+                    if(freqs[cIndex(stChar)] == 1)
                         matches--;
                 }
                 st++;
@@ -38,15 +40,29 @@ class Solution {
     }
     
     
-    Map<Character, Integer> fillFreqs(String s) {
-        Map<Character, Integer> freqs = new HashMap<>();
+    int fillFreqs(String s, int[] freqs) {
         
+        int count = 0;
         for(int i = 0 ; i < s.length() ; i++) {
             char curr = s.charAt(i);
-            freqs.put(curr, freqs.getOrDefault(curr, 0) + 1);
+            if(freqs[cIndex(curr)] == 0)
+                count++;
+            freqs[cIndex(curr)]++;
         }
         
-        return freqs;
+        for(int i = 0 ; i < 52 ; i++) {
+            if(freqs[i] == 0)
+                freqs[i] = Integer.MIN_VALUE;
+        }
+        
+        return count;
+    }
+    
+    int cIndex(char c) {
+        if(c - 'A' < 26)
+            return c - 'A' + 26;
+        else
+            return c - 'a';
     }
     
 }
