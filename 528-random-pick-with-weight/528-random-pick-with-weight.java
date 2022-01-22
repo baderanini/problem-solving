afc701b538c3fd7444b6386b62bec84e6a17be11
baderanini <anini908@gmail.com>
2022-01-23 01:00:39 +0200

    Time: 59 ms (24.96%), Space: 53.3 MB (24.84%) - LeetHub

---
+++ 528-random-pick-with-weight/528-random-pick-with-weight.java
@@ -0,0 +1,47 @@
+class Solution {
+
+    Random random;
+    int[] w;
+    int totalSum = 0;
+    public Solution(int[] w) {
+        random = new Random();
+        for(int i = 1 ; i < w.length ; i++) {
+            w[i] += w[i-1];
+        }
+        totalSum = w[w.length-1];
+        this.w = w;
+    }
+    
+    public int pickIndex() {
+        int weightedIndex = random.nextInt(totalSum) + 1;
+        int bs = binarySearch(weightedIndex);
+        return bs;
+    }
+    
+    int binarySearch(int val) {
+        int l = 0, h = w.length - 1;
+        
+        while(l <= h) {
+            int mid = l + (h-l)/2;
+            
+            if(w[mid] >= val)
+                h = mid-1;
+            else
+                l = mid+1;
+        }
+        return l;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
+
+/*
+
+| | | | | |
+
+*/
+    
