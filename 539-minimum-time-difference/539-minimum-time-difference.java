fa0836385d6af9bd074a1f41740d09838ae1a10a
baderanini <anini908@gmail.com>
2022-01-22 03:13:16 +0200

    Time: 25 ms (24.08%), Space: 47.7 MB (6.83%) - LeetHub

---
+++ 539-minimum-time-difference/539-minimum-time-difference.java
@@ -0,0 +1,73 @@
+class Time {
+    int hour;
+    int minute;
+    
+    Time(int hour, int minute) {
+        this.hour = hour;
+        this.minute = minute;
+    }
+    
+    @Override
+    public boolean equals(Object t) {
+        if(t instanceof Time) {
+            return ((Time) t).hour == this.hour && ((Time) t).minute == this.minute;
+        }
+        return false;
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.hour, this.minute);
+    }
+}
+
+
+class Solution {
+    public int findMinDifference(List<String> timePoints) {
+        Set<Time> times = new HashSet<>();
+        for(String time: timePoints)
+            if(!times.add(parseTime(time)))
+                return 0;
+            
+        
+        Time prevTime = null;
+        int minDiff = Integer.MAX_VALUE;
+        Time currTime = null, firstTime = null, lastTime = null;
+        for(int i = 0 ; i < 24 ; i++) {
+            for(int j = 0 ; j < 60 ; j++) {
+                currTime = new Time(i, j);
+                if(times.contains(currTime)) {
+                    lastTime = currTime;
+                    if(prevTime != null)
+                        minDiff = Math.min(minDiff, calcDiff(currTime, prevTime));
+                    else
+                        firstTime = currTime;
+                    prevTime = currTime;
+                }
+            }
+        }
+        
+        if(!currTime.equals(lastTime)) {
+            minDiff = Math.min(minDiff, calcDiff(lastTime, firstTime));
+        }
+        return minDiff;
+    }
+    
+    int postProcess(int diff) {
+        if(diff > 24*60/2)
+            return 24*60 - diff;
+        return diff;
+    }
+    
+    int calcDiff(Time time, Time prev) {
+        if(time.hour == prev.hour)
+            return (postProcess(time.minute - prev.minute));
+        else
+            return postProcess((time.hour - prev.hour - 1) * 60 + (time.minute + 60 - prev.minute));
+    } 
+    
+    Time parseTime(String timeString) {
+        String[] tokens = timeString.split(":");
+        return new Time(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+    }
+}
